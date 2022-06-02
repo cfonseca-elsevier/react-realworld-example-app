ba2651fdd653adf96fd254982ea7d00a1d73e636
Carlos Fonseca <52412017+cfonseca-elsevier@users.noreply.github.com>
2022-06-02 13:05:57 +0200

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,23 @@ module.exports = {
     // ...
     assert: {
       preset: 'lighthouse:recommended',
+      assertions: {
+        'uses-rel-preconnect' : 'off', 
+        'unused-javascript' : 'off', 
+        'unused-css-rules' : 'off', 
+        'themed-omnibox' : 'off', 
+        'splash-screen' : 'off', 
+        'service-worker' : 'off', 
+        'maskable-icon' : 'off', 
+        'link-name' : 'off', 
+        'installable-manifest' : 'off', 
+        'image-size-responsive' : 'off', 
+        'image-alt' : 'off', 
+        'font-display' : 'off', 
+        'csp-xss' : 'off', 
+        'color-contrast' : 'off', 
+        'apple-touch-icon' : 'off',
+      }, 
     },
     // ...
   },
